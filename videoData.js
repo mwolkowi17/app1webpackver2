7569f5c44d43ec535c6de7e2bfdf378dfeced8b6
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-02-12 08:40:17 +0100

    first

---
+++ videoData.js
@@ -0,0 +1,6 @@
+export const videoSources = [
+    "./images/Dopalacze1_Wplyw_WERSJA2popr2-Ramsar_3000.mp4",
+  // "https://drive.google.com/file/d/17mEJNpHIK1ySAdpk2Kwfkm1amet4o0TO/view?usp=sharing",
+    "./images/Dopalacze1_Wplyw_WERSJA2popr2cz2.mp4",
+    "./images/lekarz_PIERWSZA_POMOC.mp4"
+]
